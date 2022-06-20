a07526490ef39492efa976bb677aa8a50e5e19b5
y2Dev <dub-y@tuta.io>
2022-06-20 12:16:48 -0400

    affectation des variables tags à chaque variable events

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,10 +16,12 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $tags = \App\Models\Tag::factory(8)->create();
 
-        \App\Models\User::factory(10)->create()->each(function ($user){
+        \App\Models\User::factory(10)->create()->each(function ($user) use($tags) {
             \App\Models\Event::factory(rand(2, 5))->create([
                 'user_id' => $user->id
-            ])
+            ])->each(function ($event) use($tags) {
+                $event->tags()->attach($tags->random(3));
+            });
         });
     }
 }
