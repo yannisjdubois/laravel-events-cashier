61dd2874779ba9794482f86b34d42945c72d8a63
y2Dev <dub-y@tuta.io>
2022-06-21 14:55:39 -0400

    mise en français du langage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        //changement de langage
         \Carbon\Carbon::setLocale('fr');
     }
 }
