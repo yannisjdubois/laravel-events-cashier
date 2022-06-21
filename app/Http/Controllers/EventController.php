c270a9d40c1fc440ae9b794feafb707a1e5b95a5
y2Dev <dub-y@tuta.io>
2022-06-21 15:24:24 -0400

    mise en place de la fonction __construct pour sélectionner ce que le grand public peut voir

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 
 class EventController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', ['only' => ['create', 'store']]);
+    }
     /**
      * Display a listing of the resource.
      *
