4c009d4886d2df50def1a7af5a8fdef9f319481b
y2Dev <dub-y@tuta.io>
2022-06-21 16:15:42 -0400

    création d'un formulaire dans la vue create

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,3 +1,15 @@
 <x-app-layout>
+    <div class="container mx-auto">
+        <form action="{{ route('event.store') }}" method="post">
+            @csrf
     
+            <x-label for="title" value="Titre" />
+            <x-input id="title" name="title" type="text" />
+
+            <x-label for="content" value="Contenu" />
+            <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    
+        </form>
+    </div>
+
 </x-app-layout>
